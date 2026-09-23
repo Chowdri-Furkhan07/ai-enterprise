5c26b64f631ba464bbbf71135d723942a7376501
Chowdri Furkhan <mrfurkhanfurkhan@gmail.com>
2026-09-23 07:01:40 +0530

    Delete run_query function from mysql.py
    Remove the run_query function and its dependencies.

--- database/mysql.py
+++ database/mysql.py
@@ -1,10 +1,1 @@
-import mysql.connector
-from app.config import MYSQL_CONFIG
 
-def run_query(query):
-    conn = mysql.connector.connect(**MYSQL_CONFIG)
-    cur = conn.cursor()
-    cur.execute(query)
-    res = cur.fetchall()
-    conn.close()
-    return res
