6d2ea029fd887943a9a2fc9e264fd22855bffdca
Chowdri Furkhan <mrfurkhanfurkhan@gmail.com>
2026-09-26 11:58:53 +0530

    Remove tool_router function from mcp_tools.py
    Removed the tool_router function that handled SQL and search actions.

--- tools/mcp_tools.py
+++ tools/mcp_tools.py
@@ -1,6 +1,1 @@
-def tool_router(action, input):
-    if action == "sql":
-        from database.mysql import run_query
-        return run_query(input)
-    elif action == "search":
-        return f"Searching {input}"
+
